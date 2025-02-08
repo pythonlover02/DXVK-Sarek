3211b7ae875b84baf02b32ca1f3f031c350f7999
pythonlover02 <pythonloverv2@gmail.com>
2025-02-08 14:55:43 -0300

    [meta] rework the package-release.sh

--- package-release.sh
+++ package-release.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-
 shopt -s extglob
 
 if [ -z "$1" ] || [ -z "$2" ]; then
@@ -10,9 +9,9 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 fi
 
 DXVK_VERSION="$1"
-DXVK_SRC_DIR=`dirname $(readlink -f $0)`
-DXVK_BUILD_DIR=$(realpath "$2")"/dxvk-$DXVK_VERSION"
-DXVK_ARCHIVE_PATH=$(realpath "$2")"/dxvk-$DXVK_VERSION.tar.gz"
+DXVK_SRC_DIR="$(dirname "$(readlink -f "$0")")"
+DXVK_BUILD_DIR="$(realpath "$2")/dxvk-$DXVK_VERSION"
+DXVK_ARCHIVE_PATH="$(realpath "$2")/dxvk-$DXVK_VERSION.tar.gz"
 
 if [ -e "$DXVK_BUILD_DIR" ]; then
   echo "Build directory $DXVK_BUILD_DIR already exists"
@@ -49,7 +48,7 @@ done
 function build_arch {
   export WINEARCH="win$1"
   export WINEPREFIX="$DXVK_BUILD_DIR/wine.$1"
-  
+
   cd "$DXVK_SRC_DIR"
 
   opt_strip=
@@ -70,16 +69,34 @@ function build_arch {
   ninja install
 
   if [ $opt_devbuild -eq 0 ]; then
-    # get rid of some useless .a files
-    rm "$DXVK_BUILD_DIR/x$1/"*.!(dll)
-    rm -R "$DXVK_BUILD_DIR/build.$1"
+    # Ensure DLLs are present before packaging
+    if ls "$DXVK_BUILD_DIR/x$1/"*.dll 1> /dev/null 2>&1; then
+      echo "DLLs found in x$1 directory."
+    else
+      echo "Warning: No DLLs found in x$1 directory!"
+    fi
+
+    # Remove unnecessary .a files
+    rm -f "$DXVK_BUILD_DIR/x$1/"*.!(dll)
+    rm -rf "$DXVK_BUILD_DIR/build.$1"
   fi
 }
 
 function package {
-  cd "$DXVK_BUILD_DIR/.."
-  tar -czf "$DXVK_ARCHIVE_PATH" "dxvk-$DXVK_VERSION"
-  rm -R "dxvk-$DXVK_VERSION"
+  cd "$(dirname "$DXVK_BUILD_DIR")"
+
+  # Ensure DLLs exist before packaging
+  if find "$DXVK_BUILD_DIR" -name "*.dll" | grep -q .; then
+    echo "Packaging DXVK build..."
+    tar -czf "$DXVK_ARCHIVE_PATH" "$(basename "$DXVK_BUILD_DIR")"
+    echo "Package created at: $DXVK_ARCHIVE_PATH"
+  else
+    echo "Error: No DLL files found! Packaging aborted."
+    exit 1
+  fi
+
+  # Clean up after packaging
+  rm -rf "$DXVK_BUILD_DIR"
 }
 
 build_arch 64
