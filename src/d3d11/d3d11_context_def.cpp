7bfeb96b87a8e1a7003495c9b774b64627189813
pythonlover02 <pythonloverv2@gmail.com>
2025-10-04 16:08:36 -0300

    [d3d11] Fix error return on MAP_WRITE_NO_OVERWRITE on deferred context

--- src/d3d11/d3d11_context_def.cpp
+++ src/d3d11/d3d11_context_def.cpp
@@ -2,7 +2,7 @@
 #include "d3d11_device.h"
 
 namespace dxvk {
-  
+
   D3D11DeferredContext::D3D11DeferredContext(
           D3D11Device*    pParent,
     const Rc<DxvkDevice>& Device,
@@ -12,18 +12,18 @@ namespace dxvk {
     m_commandList (CreateCommandList()) {
     ClearState();
   }
-  
-  
+
+
   D3D11_DEVICE_CONTEXT_TYPE STDMETHODCALLTYPE D3D11DeferredContext::GetType() {
     return D3D11_DEVICE_CONTEXT_DEFERRED;
   }
-  
-  
+
+
   UINT STDMETHODCALLTYPE D3D11DeferredContext::GetContextFlags() {
     return m_contextFlags;
   }
-  
-  
+
+
   HRESULT STDMETHODCALLTYPE D3D11DeferredContext::GetData(
           ID3D11Asynchronous*               pAsync,
           void*                             pData,
@@ -115,8 +115,8 @@ namespace dxvk {
     if (!std::exchange(s_errorShown, true))
       Logger::warn("D3D11: Flush1 called on a deferred context");
   }
-  
-  
+
+
   HRESULT STDMETHODCALLTYPE D3D11DeferredContext::Signal(
           ID3D11Fence*                pFence,
           UINT64                      Value) {
@@ -147,16 +147,16 @@ namespace dxvk {
     D3D10DeviceLock lock = LockContext();
 
     FlushCsChunk();
-    
+
     static_cast<D3D11CommandList*>(pCommandList)->EmitToCommandList(m_commandList.ptr());
-    
+
     if (RestoreContextState)
       RestoreState();
     else
       ClearState();
   }
-  
-  
+
+
   HRESULT STDMETHODCALLTYPE D3D11DeferredContext::FinishCommandList(
           BOOL                RestoreDeferredContextState,
           ID3D11CommandList   **ppCommandList) {
@@ -164,22 +164,22 @@ namespace dxvk {
 
     FinalizeQueries();
     FlushCsChunk();
-    
+
     if (ppCommandList != nullptr)
       *ppCommandList = m_commandList.ref();
     m_commandList = CreateCommandList();
-    
+
     if (RestoreDeferredContextState)
       RestoreState();
     else
       ClearState();
-    
+
     m_mappedResources.clear();
     ResetStagingBuffer();
     return S_OK;
   }
-  
-  
+
+
   HRESULT STDMETHODCALLTYPE D3D11DeferredContext::Map(
           ID3D11Resource*             pResource,
           UINT                        Subresource,
@@ -190,7 +190,7 @@ namespace dxvk {
 
     if (unlikely(!pResource || !pMappedResource))
       return E_INVALIDARG;
-    
+
     if (MapType == D3D11_MAP_WRITE_DISCARD) {
       D3D11_RESOURCE_DIMENSION resourceDim;
       pResource->GetType(&resourceDim);
@@ -199,12 +199,12 @@ namespace dxvk {
       HRESULT status = resourceDim == D3D11_RESOURCE_DIMENSION_BUFFER
         ? MapBuffer(pResource,              &mapInfo)
         : MapImage (pResource, Subresource, &mapInfo);
-      
+
       if (unlikely(FAILED(status))) {
-        *pMappedResource = D3D11_MAPPED_SUBRESOURCE();
+        pMappedResource->pData = nullptr;
         return status;
       }
-      
+
       AddMapEntry(pResource, Subresource, resourceDim, mapInfo);
       *pMappedResource = mapInfo;
       return S_OK;
@@ -212,30 +212,30 @@ namespace dxvk {
       // The resource must be mapped with D3D11_MAP_WRITE_DISCARD
       // before it can be mapped with D3D11_MAP_WRITE_NO_OVERWRITE.
       auto entry = FindMapEntry(pResource, Subresource);
-      
+
       if (unlikely(!entry)) {
-        *pMappedResource = D3D11_MAPPED_SUBRESOURCE();
-        return E_INVALIDARG;
+        pMappedResource->pData = nullptr;
+        return D3D11_ERROR_DEFERRED_CONTEXT_MAP_WITHOUT_INITIAL_DISCARD;
       }
-      
+
       // Return same memory region as earlier
       *pMappedResource = entry->MapInfo;
       return S_OK;
     } else {
       // Not allowed on deferred contexts
-      *pMappedResource = D3D11_MAPPED_SUBRESOURCE();
+      pMappedResource->pData = nullptr;
       return E_INVALIDARG;
     }
   }
-  
-  
+
+
   void STDMETHODCALLTYPE D3D11DeferredContext::Unmap(
           ID3D11Resource*             pResource,
           UINT                        Subresource) {
     // No-op, updates are committed in Map
   }
-  
-  
+
+
   void STDMETHODCALLTYPE D3D11DeferredContext::UpdateSubresource(
           ID3D11Resource*                   pDstResource,
           UINT                              DstSubresource,
@@ -275,15 +275,15 @@ namespace dxvk {
           ID3D11Resource*               pResource,
           D3D11_MAPPED_SUBRESOURCE*     pMappedResource) {
     D3D11Buffer* pBuffer = static_cast<D3D11Buffer*>(pResource);
-    
+
     if (unlikely(pBuffer->GetMapMode() == D3D11_COMMON_BUFFER_MAP_MODE_NONE)) {
       Logger::err("D3D11: Cannot map a device-local buffer");
       return E_INVALIDARG;
     }
-    
+
     pMappedResource->RowPitch     = pBuffer->Desc()->ByteWidth;
     pMappedResource->DepthPitch   = pBuffer->Desc()->ByteWidth;
-    
+
     if (likely(m_csFlags.test(DxvkCsChunkFlag::SingleUse))) {
       // For resources that cannot be written by the GPU,
       // we may write to the buffer resource directly and
@@ -312,17 +312,17 @@ namespace dxvk {
         ctx->invalidateBuffer(cDstBuffer, slice);
       });
     }
-    
+
     return S_OK;
   }
-  
-  
+
+
   HRESULT D3D11DeferredContext::MapImage(
           ID3D11Resource*               pResource,
           UINT                          Subresource,
           D3D11_MAPPED_SUBRESOURCE*     pMappedResource) {
     D3D11CommonTexture* pTexture = GetCommonTexture(pResource);
-    
+
     if (unlikely(pTexture->GetMapMode() == D3D11_COMMON_TEXTURE_MAP_MODE_NONE)) {
       Logger::err("D3D11: Cannot map a device-local image");
       return E_INVALIDARG;
@@ -330,18 +330,18 @@ namespace dxvk {
 
     if (unlikely(Subresource >= pTexture->CountSubresources()))
       return E_INVALIDARG;
-    
+
     VkFormat packedFormat = pTexture->GetPackedFormat();
-    
+
     auto formatInfo = imageFormatInfo(packedFormat);
     auto subresource = pTexture->GetSubresourceFromIndex(
         formatInfo->aspectMask, Subresource);
-    
+
     VkExtent3D levelExtent = pTexture->MipLevelExtent(subresource.mipLevel);
-    
+
     auto layout = pTexture->GetSubresourceLayout(formatInfo->aspectMask, Subresource);
     auto dataSlice = AllocStagingBuffer(util::computeImageDataSize(packedFormat, levelExtent));
-    
+
     pMappedResource->RowPitch   = layout.RowPitch;
     pMappedResource->DepthPitch = layout.DepthPitch;
     pMappedResource->pData      = dataSlice.mapPtr(0);
@@ -351,8 +351,8 @@ namespace dxvk {
       std::move(dataSlice));
     return S_OK;
   }
-  
-  
+
+
   void D3D11DeferredContext::UpdateMappedBuffer(
           D3D11Buffer*                  pDstBuffer,
           UINT                          Offset,
@@ -398,8 +398,8 @@ namespace dxvk {
   Com<D3D11CommandList> D3D11DeferredContext::CreateCommandList() {
     return new D3D11CommandList(m_parent, m_contextFlags);
   }
-  
-  
+
+
   void D3D11DeferredContext::EmitCsChunk(DxvkCsChunkRef&& chunk) {
     m_commandList->AddChunk(std::move(chunk));
   }
