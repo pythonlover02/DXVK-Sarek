48fc86dbf52b3ce608b3812f2e9fa7c8a7a32c02
pythonlover02 <pythonloverv2@gmail.com>
2026-08-01 00:44:01 -0300

    [d3d9] document the display mode sort order

--- src/d3d9/d3d9_adapter.cpp
+++ src/d3d9/d3d9_adapter.cpp
@@ -916,8 +916,9 @@ namespace dxvk {
       m_modes.push_back(mode);
     }
 
-    // Sort display modes by width, height and refresh rate,
-    // in that order. Some games rely on correct ordering.
+    // Sort display modes by width, height and refresh rate (descending), in that order.
+    // Some games rely on correct ordering, e.g. Prince of Persia (2008) expects the highest
+    // refresh rate to be listed first for a particular resolution.
     std::sort(m_modes.begin(), m_modes.end(),
       [](const D3DDISPLAYMODEEX& a, const D3DDISPLAYMODEEX& b) {
         if (a.Width < b.Width)   return true;
