904eef11a910dbb349e1fb8bf7db7917131da17c
pythonlover02 <pythonloverv2@gmail.com>
2026-09-03 00:09:45 -0300

    [util,wsi] replace util_monitor with the win32 wsi layer

--- src/util/util_gdi.h
+++ src/util/util_gdi.h
@@ -4,6 +4,7 @@
 
 namespace dxvk {
   using NTSTATUS = LONG;
+  using D3DKMT_HANDLE = UINT;
 
   // Slightly modified definitions...
   struct D3DKMT_CREATEDCFROMMEMORY {
@@ -23,10 +24,47 @@ namespace dxvk {
     HANDLE hBitmap = nullptr;
   };
 
+  typedef enum _D3DKMT_ESCAPETYPE
+  {
+      D3DKMT_ESCAPE_UPDATE_RESOURCE_WINE = 0x80000000,
+      D3DKMT_ESCAPE_SET_PRESENT_RECT_WINE = 0x80000001,
+  } D3DKMT_ESCAPETYPE;
+
+  typedef struct _D3DDDI_ESCAPEFLAGS
+  {
+      union
+      {
+          struct
+          {
+              UINT HardwareAccess :1;
+              UINT Reserved       :31;
+          };
+          UINT Value;
+      };
+  } D3DDDI_ESCAPEFLAGS;
+
+  typedef struct _D3DKMT_ESCAPE
+  {
+      D3DKMT_HANDLE      hAdapter;
+      D3DKMT_HANDLE      hDevice;
+      D3DKMT_ESCAPETYPE  Type;
+      D3DDDI_ESCAPEFLAGS Flags;
+      void              *pPrivateDriverData;
+      UINT               PrivateDriverDataSize;
+      D3DKMT_HANDLE      hContext;
+  } D3DKMT_ESCAPE;
+
   using D3DKMTCreateDCFromMemoryType  = NTSTATUS(STDMETHODCALLTYPE*) (D3DKMT_CREATEDCFROMMEMORY*);
   NTSTATUS D3DKMTCreateDCFromMemory (D3DKMT_CREATEDCFROMMEMORY*  Arg1);
 
   using D3DKMTDestroyDCFromMemoryType = NTSTATUS(STDMETHODCALLTYPE*) (D3DKMT_DESTROYDCFROMMEMORY*);
   NTSTATUS D3DKMTDestroyDCFromMemory(D3DKMT_DESTROYDCFROMMEMORY* Arg1);
+
+  // The present rect escape is only understood by Wine's gdi32. On a host
+  // that does not export D3DKMTEscape at all, or an older Wine that does
+  // not know the escape type, this degrades to a warning and the caller
+  // carries on with plain window positioning.
+  using D3DKMTEscapeType              = NTSTATUS(STDMETHODCALLTYPE*) (const D3DKMT_ESCAPE*);
+  NTSTATUS D3DKMTEscape             (const D3DKMT_ESCAPE*        Arg1);
 
 }
